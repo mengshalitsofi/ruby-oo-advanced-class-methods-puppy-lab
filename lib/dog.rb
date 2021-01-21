7914645e5c0dd8a48e1533c3fddeb2bb1771bb40
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-20 16:30:54 -0800

    commit tree 3f7b26478a114088b709412ff0b713bb2ddda43c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save 
+    end
+
+    def self.all
+        @@all 
+
+    end
+
+    def self.print_all
+        puts @@all.map{|x| x.name}.join("\n")
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+       @@all = []
+    end
+end
